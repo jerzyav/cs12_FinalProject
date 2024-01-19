f16476938538a6bc81737c3a4965332de8d0c068
gabeprasmo <152203050+gabeprasmo@users.noreply.github.com>
2024-01-19 08:50:36 -0600

    Updated some operations

--- src/Operations.java
+++ src/Operations.java
@@ -13,22 +13,36 @@ public class Operations {
     }
 
     double divide(double x, double y) {
-        return x / y;
+        if (y != 0) {
+            return x / y;
+        } else {
+            System.out.println("Error: Division by zero.");
+            return Double.NaN;
+        }
     }
 
-    double square(double x, double y) {
+    double square(double x) {
         return x * x;
     }
 
     double square_root(double x) {
-        return Math.sqrt(x) ;
+        return Math.sqrt(x);
     }
 
     double power(double x, double y) {
-        return Math.pow(x,y);
+        return Math.pow(x, y);
     }
 
-    double factorial(double x, double y) {
-        return x * x;
+    double factorial(double x) {
+        if (x < 0) {
+            System.out.println("Error: Factorial of a negative number is undefined.");
+            return Double.NaN;
+        }
+
+        double result = 1;
+        for (int i = 2; i <= x; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
